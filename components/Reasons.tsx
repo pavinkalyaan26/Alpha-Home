10d091eaf6a7c138d9542f06e7897ac9de92e877
Pavin Kalyaan S S <pavinkalyaan263@gmail.com>
2024-07-23 20:17:35 +0530

    update

--- components/Reasons.tsx
+++ components/Reasons.tsx
@@ -6,7 +6,7 @@ export default function Reasons() {
   return (
     <section className="max-w-7xl my-20 flex flex-col items-center">
       <h1 className="text-4xl leading-tight text-center text-indigo-400 mb-10 w-[900px]">
-        More reasons companies around the world choose AlphaX
+        More reasons companies around the world choose CrackTech
       </h1>
       <div className="flex items-center justify-center max-w-5xl gap-4 flex-wrap">
         {BackUp.map((item, index) => (
